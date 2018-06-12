c470c37d10f50c04a0efc1243b3cfda88efa8061
LuizHonorato <lhehonorato@gmail.com>
2018-06-12 14:50:12 -0300

    MemoCalc3

--- memo-calc-3/src/calculator/calculator.jsx
+++ memo-calc-3/src/calculator/calculator.jsx
@@ -3,11 +3,12 @@ import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 
 import Grid from '../template/grid'
-import {key1, key2, key3, key4, key5, key6, key7, key8, key9, key0, clear, changeDisplay} from './calculatorActions'
+import {addNum, clear, equal, back} from './calculatorActions'
 
-const btns = ["±", "%", "Back", "C", "7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "/", "0", ".", "=", "*"]
+const Calculator = (props) => {
+    const btns = props.btns || []
 
-const Calculator = (props) => (
+    return(
     <div className='container'>
         <div className='row text-center'>
             <h2 className='title-page'>MemoCalc3</h2>
@@ -15,44 +16,40 @@ const Calculator = (props) => (
         <div className='box-calculator'>
             <div className='row'>
                 <Grid cols='12 9 7'>
-                    <input type="text" className="calc-area" value={props.calculator.number} onChange={props.changeDisplay} />
+                    <input type="text" className="calc-area" value={props.calculator.number} onChange={props.addNum} />
                 </Grid>
                 <Grid cols='12 9 5'>
-                    <p>Memória</p>
+                    <h3>Memória</h3>
                 </Grid>
             </div>
             <div className='row'>
                 <Grid cols='12 9 7'>
-                    <span className='calc-button'>±</span>
-                    <span className='calc-button'>%</span>
-                    <span className='calc-button'>Back</span>
-                    <span className='calc-button clear-button' onClick={props.clear}>C</span>
-
-                    <span className='calc-button' onClick={props.key7}>7</span>
-                    <span className='calc-button' onClick={props.key8}>8</span>
-                    <span className='calc-button' onClick={props.key9}>9</span>
-                    <span className='calc-button'>+</span>
-
-                    <span className='calc-button' onClick={props.key4}>4</span>
-                    <span className='calc-button' onClick={props.key5}>5</span>
-                    <span className='calc-button' onClick={props.key6}>6</span>
-                    <span className='calc-button'>-</span>
-
-                    <span className='calc-button' onClick={props.key1}>1</span>
-                    <span className='calc-button' onClick={props.key2}>2</span>
-                    <span className='calc-button' onClick={props.key3}>3</span>
-                    <span className='calc-button'>/</span>
-
-                    <span className='calc-button' onClick={props.key0}>0</span>
-                    <span className='calc-button'>.</span>
-                    <span className='calc-button'>=</span>
-                    <span className='calc-button'>*</span>
+                    {btns.map((item, key) => {
+                        if(item == "C"){
+                            return(
+                                <span className='calc-button clear-button' key={key} onClick={props.clear}>{item}</span>
+                            )
+                        } else if(item == "Back"){
+                            return (
+                                <span className='calc-button' key={key} onClick={() => props.back(props.calculator.number)}>{item}</span>
+                            )
+                        } else if(item == "="){
+                            return (
+                                <span className='calc-button' key={key} onClick={() => props.equal(props.calculator.number)}>{item}</span>
+                            )
+                        } else {
+                            return (
+                                <span className='calc-button' key={key} onClick={() => props.addNum(item)}>{item}</span>
+                            )
+                        }
+                    })}
                 </Grid>
             </div>
         </div>
     </div>
-)
+    )
+}
 
-const mapStateToProps = state => ({calculator: state.calculator})
-const mapDispatchToProps = dispatch => bindActionCreators({key1, key2, key3, key4, key5, key6, key7, key8, key9, key0, clear, changeDisplay}, dispatch)
+const mapStateToProps = state => ({calculator: state.calculator, btns: state.calculator.btns})
+const mapDispatchToProps = dispatch => bindActionCreators({addNum, clear, equal, back}, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(Calculator)
--- memo-calc-3/src/calculator/calculatorReducer.js
+++ memo-calc-3/src/calculator/calculatorReducer.js
@@ -1,30 +1,17 @@
-const INITIAL_STATE = {number: "0",
-                       btns: ["±","%","Back","C","7","8","9","+","4","5","6","-","1","2","3","/","0",".","=","*"]}
+const INITIAL_STATE = {number: 0,
+                       btns: ["±","%","Back","C","7","8","9","+","4","5","6","-","1","2","3","/","0",".","=","*"] }
 
 export default (state = INITIAL_STATE, action) => {
     switch(action.type) {
-        case 'KEY_1_PRESSED':
-            return {...state, number: "1"}
-        case 'KEY_2_PRESSED':
-            return {...state, number: "2"}
-        case 'KEY_3_PRESSED':
-            return {...state, number: "3"}
-        case 'KEY_4_PRESSED':
-            return {...state, number: "4"}
-        case 'KEY_5_PRESSED':
-            return {...state, number: "5"}
-        case 'KEY_6_PRESSED':
-            return {...state, number: "6"}
-        case 'KEY_7_PRESSED':
-            return {...state, number: "7"}
-        case 'KEY_8_PRESSED':
-            return {...state, number: "8"}
-        case 'KEY_9_PRESSED':
-            return {...state, number: "9"}
-        case 'KEY_0_PRESSED':
-            return {...state, number: "0"}
-        case 'DISPLAY_CHANGED':
-            return {...state, number: state.number += action.payload}
+        case 'NUMBER_ADDED':
+            return {...state, number: state.number == 0 ? action.payload : state.number + action.payload}
+        case 'DISPLAY_CLEAR':
+            return {...state, number: 0}
+        case 'IS_EQUAL':
+            return {...state, number: eval(action.payload).toString()}
+        case 'BACK_PRESSED':
+            var str = action.payload
+            return {...state, number: str.slice(0, -1)}
         default:
             return state
     }
