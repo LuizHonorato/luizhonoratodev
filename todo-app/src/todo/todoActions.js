9bb4f70acacb999c66c58265e5016a9bc0dc1918
LuizHonorato <lhehonorato@gmail.com>
2018-06-20 17:42:46 -0300

    ToDo App

---
+++ todo-app/src/todo/todoActions.js
@@ -0,0 +1,27 @@
+export const add = () => {
+
+}
+
+export const changeDescription = () => {
+
+}
+
+export const search = () => {
+
+}
+
+export const clear = () => {
+
+}
+
+export const markAsDone = () => {
+
+}
+
+export const markAsPending = () => {
+
+}
+
+export const remove = () => {
+    
+}
---
+++ todo-app/src/todo/todoReducer.js
@@ -0,0 +1,8 @@
+const INITIAL_STATE = { description: '', list: []}
+
+export default (state = INITIAL_STATE, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
